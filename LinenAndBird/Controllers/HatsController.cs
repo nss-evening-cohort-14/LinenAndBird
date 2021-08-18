4f69e80dd3865efb4b0783c5f1dae8c370c8d6f4
Nathan Gonzalez <ncgonz@gmail.com>
2021-08-18 09:20:58 -0500

    Add project files.

---
+++ LinenAndBird/Controllers/HatsController.cs
@@ -0,0 +1,59 @@
+﻿using LinenAndBird.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinenAndBird.Controllers
+{
+    [Route("api/hats")] //exposed at this endpoint
+    [ApiController] // an api controller, so it returns json or xml
+    public class HatsController : ControllerBase
+    {
+        static List<Hat> _hats = new List<Hat>
+            {
+                new Hat
+                {
+                    Color = "Blue",
+                    Designer = "Charlie",
+                    Style = HatStyle.OpenBack
+                },
+                new Hat
+                {
+                    Color = "Black",
+                    Designer = "Nathan",
+                    Style = HatStyle.WideBrim
+                },
+                new Hat
+                {
+                    Color = "Magenta",
+                    Designer = "Charlie",
+                    Style = HatStyle.Normal
+                }
+            };
+
+        [HttpGet]
+        public List<Hat> GetAllHats()
+        {
+            return _hats;
+        }
+
+        // GET /api/hats/styles/1 -> all open backed hats
+        [HttpGet("styles/{style}")]
+        public IEnumerable<Hat> GetHatsByStyle(HatStyle style)
+        {
+            var matches = _hats.Where(hat => hat.Style == style);
+
+            return matches;
+        }
+
+        [HttpPost]
+        public void AddAHat(Hat newHat)
+        {
+            _hats.Add(newHat);
+        }
+
+    }
+}
---
+++ LinenAndBird/Models/Hat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinenAndBird.Models
+{
+    public class Hat
+    {
+        public string Designer { get; set; }
+        public string Color { get; set; }
+        public HatStyle Style { get; set; }
+    }
+
+    public enum HatStyle
+    {
+        Normal,
+        OpenBack,
+        WideBrim
+    }
+}
